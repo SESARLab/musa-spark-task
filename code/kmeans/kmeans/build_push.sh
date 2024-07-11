e92a9ecd9236b122f20c58ed068e338a6c201d70
gamirkola <mirkogal@gmail.com>
2024-07-11 12:07:47 +0200

    mods

--- code/kmeans/kmeans/build_push.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Build the image
-$SPARK_HOME/bin/docker-image-tool.sh -r gabrielestentella -t km -f /Users/gabriele/Desktop/TesiStentella/tesistentella/kmeans/kmeans/Dockerfile build
-
-# Push the image
-docker push gabrielestentella/spark:km
--- code/test_all.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/zsh
-
-./launch_test.sh kmeans dataset.csv /dataset.csv 5 /RESULT &&
-./launch_test.sh kmeans_model ds_nonum.csv csvData=/ds_nonum.csv k=5 resultPath=/RESULT-model &&
-./launch_test.sh kmeans_predict ds_nonum.csv csvData=/ds_nonum.csv k=5 modelData=/RESULT-model resultPath=/RESULT-predict &&
-./launch_test.sh bkmeans dataset.csv /dataset.csv 5 /RESULT &&
-./launch_test.sh bkmeans_model ds_nonum.csv csvData=/ds_nonum.csv k=5 resultPath=/RESULT-model &&
-./launch_test.sh bkmeans_predict ds_nonum.csv csvData=/ds_nonum.csv k=5 modelData=/RESULT-model resultPath=/RESULT-predict &&
-echo -e "\e[32mSUCCESS\e[0m"
