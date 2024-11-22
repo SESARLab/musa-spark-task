bb76e28784d092e3227f950cba3226e2275f33da
gamirkola <mirkogal@gmail.com>
2024-11-22 12:36:16 +0100

    fix formatting and split output into 4 different files

--- code/lr/lr/src/main/java/it/unimi/evotion/tasks/lr/LRegTask.java
+++ code/lr/lr/src/main/java/it/unimi/evotion/tasks/lr/LRegTask.java
@@ -19,7 +19,6 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-
 public class LRegTask implements Task {
 
     // ----------------------------------------------------------------------
@@ -30,14 +29,14 @@ public class LRegTask implements Task {
     private String csvData;
     private String labelName;
     private String label;
-    private int    labelIndex;
+    private int labelIndex;
     private String resultPath;
     private String maxIter;
     private String regParam;
     private String elasticNetParam;
     private List<String> lcn = new ArrayList<>();
 
-    private Dataset<Row> df;    // input dataframe
+    private Dataset<Row> df; // input dataframe
     private Dataset<Row> df2;
     private Dataset<Row> df3;
     private Dataset<Row> dfa;
@@ -60,12 +59,14 @@ public class LRegTask implements Task {
      * for regressione, otherwise (int) for classification
      *
      * @param args
-     *      args[0] url_csv input dataset (in CSV with header format)
-     *      args[1] name|index (0-based) of the dependent (label) column/variable
-     *      args[2] name|index (0-based) of the dependent (label) column/variable
-     *      args[3] number of maximum iterations
-     *      args[4] regression parameter
-     *      args[5] url_json|url_csv result of Linear Regression information
+     *             args[0] url_csv input dataset (in CSV with header format)
+     *             args[1] name|index (0-based) of the dependent (label)
+     *             column/variable
+     *             args[2] name|index (0-based) of the dependent (label)
+     *             column/variable
+     *             args[3] number of maximum iterations
+     *             args[4] regression parameter
+     *             args[5] url_json|url_csv result of Linear Regression information
      * @throws Exception
      */
     @Override
@@ -88,12 +89,11 @@ public class LRegTask implements Task {
         try {
             this.spark = SparkSession
                     .builder()
-                    //.master("local[2]")
+                    // .master("local[2]")
                     // .master("yarn")
                     .appName("LReg")
                     .getOrCreate();
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             this.spark = SparkSession
                     .builder()
                     .master("local[2]")
@@ -113,32 +113,30 @@ public class LRegTask implements Task {
                 .option("inferSchema", "true")
                 .load(csvData);
 
-       // df = df.withColumn("label1", regexp_replace(df.col(this.label), "'", ""));
-       // df = df.withColumn("label", (df.col("label1")
-                //.cast("Double")
-       // ))
-        //        .drop("label1");
+        // df = df.withColumn("label1", regexp_replace(df.col(this.label), "'", ""));
+        // df = df.withColumn("label", (df.col("label1")
+        // .cast("Double")
+        // ))
+        // .drop("label1");
         convertFeatures();
 
         StringIndexer indexer = new StringIndexer()
                 .setInputCol(String.valueOf(df.col(this.label)))
                 .setOutputCol("label");
         Dataset<Row> dfi = indexer.fit(df).transform(df);
-        //dfi.show();
+        // dfi.show();
 
         String processedLabelNames = this.labelName.replaceAll("'", "");
 
         df2 = dfi;
-        for(String c : df2.columns()) {
+        for (String c : df2.columns()) {
             df2 = df2.withColumnRenamed(c, c.replaceAll("'", ""));
         }
         df2.createOrReplaceTempView("dft");
         df3 = spark.sql(String.format("SELECT %s, label FROM dft", processedLabelNames));
 
-
         this.lcn = Arrays.stream(processedLabelNames.split(",")).collect(Collectors.toList());
         String[] names = this.lcn.stream().toArray(String[]::new);
-
 
         VectorAssembler assembler1 = new VectorAssembler()
                 .setInputCols(names)
@@ -150,30 +148,29 @@ public class LRegTask implements Task {
 
     @Override
     public void postProcessing(Object... params) throws Exception {
-
         this.dfResult.coalesce(1).write()
                 .format("csv")
-                .mode(SaveMode.Overwrite)
+                // .mode(SaveMode.Overwrite)
                 .option("header", "true")
-                .save(resultPath);
+                .save(resultPath + "-summary");
 
         this.dfResult2.coalesce(1).write()
                 .format("csv")
-                .mode(SaveMode.Append)
+                // .mode(SaveMode.Overwrite)
                 .option("header", "true")
-                .save(resultPath);
+                .save(resultPath + "-residuals");
 
         this.dfResult3.coalesce(1).write()
                 .format("csv")
-                .mode(SaveMode.Append)
+                // .mode(SaveMode.Overwrite)
                 .option("header", "true")
-                .save(resultPath);
+                .save(resultPath + "-objective-history");
 
         this.dfResult4.coalesce(1).write()
                 .format("csv")
-                .mode(SaveMode.Append)
+                // .mode(SaveMode.Overwrite)
                 .option("header", "true")
-                .save(resultPath);
+                .save(resultPath + "-coefficients");
 
         this.logger.info(this.debugString);
 
@@ -184,13 +181,13 @@ public class LRegTask implements Task {
     private void convertFeatures() {
         List<String> stringFeatures = new ArrayList<>();
 
-        for(StructField field : this.df.schema().fields()) {
+        for (StructField field : this.df.schema().fields()) {
             if (field.dataType().equals(DataTypes.StringType))
                 stringFeatures.add(field.name());
         }
 
-        for(String sf : stringFeatures) {
-            String rn = "str-"+sf;
+        for (String sf : stringFeatures) {
+            String rn = "str-" + sf;
             this.df = this.df.withColumnRenamed(sf, rn);
             StringIndexer encoder = new StringIndexer()
                     .setInputCol(rn)
@@ -219,7 +216,7 @@ public class LRegTask implements Task {
                         trainingSummary.rootMeanSquaredError(), trainingSummary.r2()));
 
         StructType scheman;
-        scheman = new StructType(new StructField[]{
+        scheman = new StructType(new StructField[] {
                 new StructField("Intercept", DataTypes.DoubleType, true, Metadata.empty()),
                 new StructField("Number_Of_Iterations", DataTypes.IntegerType, true, Metadata.empty()),
                 new StructField("RMSE", DataTypes.DoubleType, true, Metadata.empty()),
@@ -229,9 +226,9 @@ public class LRegTask implements Task {
         dfResult = spark.createDataFrame(rowsn, scheman);
         dfResult2 = trainingSummary.residuals();
 
-        StructType schema3 = new StructType(new StructField[]{
+        StructType schema3 = new StructType(new StructField[] {
                 new StructField("Coefficients", DataTypes.DoubleType, true, Metadata.empty())
-        }   );
+        });
 
         List<Row> rows3 = new ArrayList<>();
 
@@ -241,24 +238,24 @@ public class LRegTask implements Task {
         List<String> sl = Arrays.stream(s.split(",")).collect(Collectors.toList());
         List<Double> sli = new ArrayList<Double>(sl.size());
 
-        for(String str : sl) {
+        for (String str : sl) {
             sli.add(Double.valueOf(str));
         }
 
         Double[] sla = sli.stream().toArray(Double[]::new);
 
-        for(int i = 0; i < sla.length; i++){
+        for (int i = 0; i < sla.length; i++) {
             rows3.add(RowFactory.create(sla[i]));
         }
 
         dfResult4 = spark.createDataFrame(rows3, schema3);
 
-        StructType schema2 = new StructType(new StructField[]{
+        StructType schema2 = new StructType(new StructField[] {
                 new StructField("Objective_History", DataTypes.DoubleType, true, Metadata.empty())
-        }   );
+        });
 
         List<Row> r = new ArrayList<>();
-        for(int i = 0; i < trainingSummary.objectiveHistory().length; i++){
+        for (int i = 0; i < trainingSummary.objectiveHistory().length; i++) {
             r.add(RowFactory.create(trainingSummary.objectiveHistory()[i]));
         }
 
